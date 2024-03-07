44930799b2317ab22bcb131d96e5e82d88a238cf
surya-selvakumar <surya8428sm@gmail.com>
2024-03-07 17:33:14 +0530

    version5:33"

--- app.py
+++ app.py
@@ -275,6 +275,6 @@ def adminDashboard():
 
 
 if __name__ == '__main__':
-    preprocessor = joblib.load('./models/food_wastage_preprocessor_updated.joblib')
+    preprocessor = joblib.load('./models/preprocess.joblib')
     model = joblib.load('./models/model.joblib')
     app.run(debug=True)
